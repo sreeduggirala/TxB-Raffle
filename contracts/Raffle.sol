53ea2c0d2496886d577bceec5d3308445b3a0aa5
Sree Duggirala <sreevanthduggirala@gmail.com>
2023-02-05 10:37:13 -0600

    restoring earlier version

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
 
 // Custom Errors
@@ -24,9 +25,7 @@ error NoRaffleForThisNFT();
 error NoRaffleForThisID();
 
 // contract shouldn't be abstract once Chainlink is implemented
-abstract contract Raffle is VRFV2WrapperConsumerBase {
-    // Contract Owner
-    address payable public owner;
+abstract contract Raffle is VRFV2WrapperConsumerBase, Ownable {
     
     // Raffle Content
     address payable immutable nftOwner;
@@ -54,7 +53,6 @@ abstract contract Raffle is VRFV2WrapperConsumerBase {
 
     constructor(uint256 _ticketFee, uint256 _minTickets, uint256 _startTime, 
     uint256 _endTime, address _nftContract, uint256 _nftID) {
-        owner = payable(address(0x8B603f2890694cF31689dFDA28Ff5e79917243e9));
         nftOwner = payable(msg.sender);
         ticketFee = _ticketFee;
         minTickets = _minTickets;
@@ -84,13 +82,6 @@ abstract contract Raffle is VRFV2WrapperConsumerBase {
             revert WinnerAlreadyChosen();
             _;
         }
-    }
-
-    modifier onlyOwner() {
-        if(msg.sender != owner) {
-            revert NotOwner();
-        }
-        _;
     }
 
     // Enter the NFT raffle
@@ -140,7 +131,9 @@ abstract contract Raffle is VRFV2WrapperConsumerBase {
     function fulfillRandomness() external {} //Chainlink
 
     function disbursement() external nftHeld { ///automatically occurrs when time runs out
-        if(randomNumber == type(uint256).max) {
+        // Theoretically, the randomNumber can be 0, and if it is, the person who bought the first ticket will be pretty mad.
+      // maybe initialize randomNumber to uint256(-1).
+        if(randomNumber == 0) {
             revert RandomNumberStillLoading();
         }
 
@@ -164,10 +157,6 @@ abstract contract Raffle is VRFV2WrapperConsumerBase {
             revert ContractNotHoldingNFT();
         }
 
-        payable(owner).transfer((address(this).balance));
-    }
-
-    function reappointOwner(address payable _newOwner) external onlyOwner {
-        owner = payable(_newOwner);
+        payable(owner()).transfer((address(this).balance));
     }
 }
